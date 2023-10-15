99e952db7e8646012ce8007f54d740fa526d3224
LucasDev <LucasDev9645@gmail.com>
2023-10-15 11:29:51 -0300

    Fazendo testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+import PostComment from ".";
+
+describe("Teste para o componente PostComment", () => {
+  test("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument(); //botao
+  });
+
+  test("Adicionar 2 comentários de teste", () => {
+    render(<PostComment />);
+    fireEvent.change(screen.getByTestId("comment-field"), {
+      target: {
+        value: "Comentário de teste 1",
+      },
     });
+    fireEvent.click(screen.getByTestId("comment-button"));
+
+    fireEvent.change(screen.getByTestId("comment-field"), {
+      target: {
+        value: "Comentario de teste 2",
+      },
+    });
+    fireEvent.click(screen.getByTestId("comment-button"));
+    expect(screen.getAllByTestId("comment")).toHaveLength(2);
+  });
 });
